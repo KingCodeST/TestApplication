9f30c30ba2085d07eb3f105c4846a2f60797096f
Siphokuhle <cyfotyasi@gmail.com>
2020-01-20 00:40:56 +0200

    ADD FEATURED

---
+++ src/main/java/org/example/factory/institution/InstitutionTypeFactory.java
@@ -0,0 +1,18 @@
+package org.example.factory.institution;
+
+import org.example.domain.institution.InstitutionType;
+
+public class InstitutionTypeFactory {
+
+    public static InstitutionType GenericBuilder(String institutionId,String institutionTypeId,String parentInstitutionId)
+    {
+        return new InstitutionType.Builder()
+                                  .setInstitutionId(institutionId)
+                                  .setInstitutionTypeName(institutionTypeId)
+                                    .setParentInstitutionTypeId(parentInstitutionId)
+                                    .build();
+
+    }
+
+    
+}
