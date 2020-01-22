2246faa1270affbbbc23e5a76fd55ecda966df7c
KingCodeST <cyfotyasi@gmail.com>
2020-01-22 22:23:01 +0200

    add features from mac

--- src/main/java/org/example/repository/driver/DriverRepository.java
+++ src/main/java/org/example/repository/driver/DriverRepository.java
@@ -1,9 +1,9 @@
-package org.example.repository.driver;
-
-import org.example.domain.driver.Driver;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DriverRepository extends JpaRepository<Driver,String> {
-}
+//package org.example.repository.driver;
+//
+//import org.example.domain.driver.UserDriver;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface DriverRepository extends JpaRepository<UserDriver,String> {
+//}
