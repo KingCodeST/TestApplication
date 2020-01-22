4c24a6ea8147db4492397dbeb5964d3eda310ee7
Siphokuhle <cyfotyasi@gmail.com>
2020-01-22 22:16:41 +0200

    ADD FEATURED

--- src/main/java/org/example/service/location/impl/LocationTypeServiceImpl.java
+++ src/main/java/org/example/service/location/impl/LocationTypeServiceImpl.java
@@ -1,12 +1,20 @@
 package org.example.service.location.impl;
 
 import org.example.domain.location.LocationType;
+import org.example.repository.location.LocationTypeRepository;
 import org.example.service.location.LocationTypeService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LocationTypeServiceImpl implements LocationTypeService {
 
+    private static LocationTypeService locationTypeService= null;
+    private LocationTypeRepository locationTypeRepository;
+
+    private LocationTypeServiceImpl()
+    {
+
+    }
 
     @Override
     public LocationType create(LocationType locationType) {
