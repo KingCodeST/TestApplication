bf8e766fc95103ea7936f097463a0135fe469a72
KingCodeST <cyfotyasi@gmail.com>
2020-02-09 19:21:59 +0200

    Merge branch 'master' of https://github.com/KingCodeST/TestApplication # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- src/test/java/org/example/service/location/impl/LocationServiceImplTest.java
+++ src/test/java/org/example/service/location/impl/LocationServiceImplTest.java
@@ -1,10 +1,17 @@
 package org.example.service.location.impl;
 
+import org.example.domain.driver.Driver;
+import org.example.domain.location.Location;
+import org.example.factory.location.LocationFactory;
+import org.example.repository.location.LocationRepository;
+import org.example.service.location.LocationService;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -15,9 +22,11 @@ import static org.junit.Assert.*;
 @FixMethodOrder(MethodSorters.JVM)
 public class LocationServiceImplTest {
 
-    @Before
-    public void setUp() throws Exception {
-    }
+    @Autowired
+    private  LocationService service;
+    private LocationRepository repository;
+
+    Location location= LocationFactory.builderDriver("#T454","Catsville Residence","18.4590","-33.9382","");
 
     @Test
     public void getLocationService() {
@@ -33,6 +42,10 @@ public class LocationServiceImplTest {
 
     @Test
     public void create() {
+        Location create =this.service.create(this.location);
+        System.out.println("in create, created = "+create);
+        Assert.assertNotNull(create);
+        System.out.println(this.location);
     }
 
     @Test
