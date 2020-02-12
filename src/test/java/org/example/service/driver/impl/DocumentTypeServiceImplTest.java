810887a06aca13937c192a1d1de1d1ed291e0ea6
KingCodeST <cyfotyasi@gmail.com>
2020-02-12 02:03:04 +0200

    Merge branch 'master' of https://github.com/KingCodeST/TestApplication # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- src/test/java/org/example/service/driver/impl/DocumentTypeServiceImplTest.java
+++ src/test/java/org/example/service/driver/impl/DocumentTypeServiceImplTest.java
@@ -1,9 +1,17 @@
 package org.example.service.driver.impl;
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
 
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@FixMethodOrder(MethodSorters.JVM)
 public class DocumentTypeServiceImplTest {
 
     @Test
--- src/test/java/org/example/service/location/impl/LocationTypeServiceImplTest.java
+++ src/test/java/org/example/service/location/impl/LocationTypeServiceImplTest.java
@@ -1,10 +1,33 @@
 package org.example.service.location.impl;
 
+import org.example.domain.location.LocationType;
+import org.example.factory.location.LocationTypeFactory;
+import org.example.repository.location.LocationTypeRepository;
+import org.example.service.location.LocationTypeService;
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
 
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@FixMethodOrder(MethodSorters.JVM)
 public class LocationTypeServiceImplTest {
+
+    private static LocationTypeService service;
+
+    @Autowired
+    private LocationTypeRepository repository;
+
+    LocationType locationType= LocationTypeFactory.builderDriver("Campus","CPUT");
+
+
 
     @Test
     public void getLocationTypeService() {
@@ -12,6 +35,10 @@ public class LocationTypeServiceImplTest {
 
     @Test
     public void create() {
+        LocationType create=this.service.create(this.locationType);
+        System.out.println("locationtype was created = "+create);
+        Assert.assertNotNull(create);
+        System.out.println(this.locationType);
     }
 
     @Test
